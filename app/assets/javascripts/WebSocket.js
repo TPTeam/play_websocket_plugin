456feb58133df4b4900ef13011e332c6e3612930
pietroTP <pietro.grandi.3d@gmail.com>
2014-07-18 15:05:17 +0200

    some refactoring

--- app/assets/javascripts/WebSocket.js
+++ app/assets/javascripts/WebSocket.js
@@ -1,231 +1,191 @@
 var WS = WS || {};
 
-(function(func) {
-	if (typeof require === 'function' && typeof define === 'function') {
-		// define module, put dependencies here
-		define([ '' ], func);
-	} else {
-		// go ahead with global scope pollution :-)
-		WS = func();
-	}
-})(function() {
+(function (func) {
+    if (typeof require === 'function' && typeof define === 'function') {
+        // define module, put dependencies here
+        define([ '' ], func);
+    } else {
+        // go ahead with global scope pollution :-)
+        WS = func();
+    }
+})(function () {
 
-	/**
-	 * @preserve Copyright 2013 mrDOob.
-	 *           https://github.com/mrdoob/eventtarget.js/ THankS mr DOob!
-	 */
-	var EventTarget = function() {
+    var TIMEOUT = 2000;
 
-		/*
-		 * event = { type: 'eventName', content : data }
-		 */
+    var WS = function (_webSocketId, _placeholderCssSel, verbose) {
 
-		var listeners = {};
+        this.listeners = {};
+        
+        this.id = _webSocketId;
+        this.placeholderCssSel = _placeholderCssSel;
 
-		/**
-		 * {function(string, function)}
-		 */
-		this.addEventListener = this.on = function(type, listener) {
+        this.address = "";
 
-			if (listeners[type] === undefined) {
+        this.wsSocket = null;
+        this.hangOutCheck = null;
+        this.pong = JSON.stringify({
+            "pong" : true
+        })
 
-				listeners[type] = [];
+        this.verbose = (verbose !== undefined) ? verbose : false;
+    }
 
-			}
+    WS.prototype.off = function (type, listener) {
 
-			if (listeners[type].indexOf(listener) === -1) {
+        if (this.listeners[type] !== undefined) {
 
-				listeners[type].push(listener);
-			}
+            var index = this.listeners[type].indexOf(listener);
 
-		};
+            if (index !== -1) {
 
-		this.dispatchEvent = this.emit = function(event) {
+                this.listeners[type].splice(index, 1);
 
-			for ( var listener in listeners[event.type]) {
+            }
 
-				listeners[event.type][listener](event);
+        }
 
-			}
+    };
 
-		};
+    WS.prototype.on = function (type, listener) {
 
-		this.removeEventListener = this.off = function(type, listener) {
-			
-			if (listeners[type] !== undefined) {
+        if (this.listeners[type] === undefined) {
 
-				var index = listeners[type].indexOf(listener);
-	
-				if (index !== -1) {
-	
-					listeners[type].splice(index, 1);
-	
-				}
-				
-			}
+            this.listeners[type] = [];
 
-		};
+        }
 
-	};
+        if (this.listeners[type].indexOf(listener) === -1) {
 
-	var WS = function(_webSocketId, _placeholderCssSel, verbose) {
+            this.listeners[type].push(listener);
+        }
 
-		EventTarget.call(this);
+    };
 
-		var that = this;
-		
-		this.id = _webSocketId;
-		this.placeholderCssSel = _placeholderCssSel;
+    WS.prototype.emit = function (event) {
 
-		this.address = "";
+        for ( var listener in this.listeners[event.type]) {
 
-		this.wsSocket = null;
-		this.hangOutCheck = null;
-		this.pong = JSON.stringify({
-			"pong" : true
-		})
+            this.listeners[event.type][listener](event);
 
-//		this.autoReconnect = false;
-		this.verbose = (verbose !== undefined) ? verbose : false;
-	}
+        }
 
-	WS.prototype = {
+    };
 
-		isReady : function() {
-			return (this.wsSocket && this.wsSocket.readyState === 1);
-		},
+    WS.prototype.isReady = function () {
+        return (this.wsSocket && this.wsSocket.readyState === 1);
+    };
 
-		close : function(msg) {
-			if (this.verbose) {
-				console.log("WebSocket: " + this.id + " closing by me");
-			}
-			this.wsSocket.close();
-		},
+    WS.prototype.close = function (msg) {
+        if (this.verbose) {
+            console.log("WebSocket: " + this.id + " closing by me");
+        }
+        this.wsSocket.close();
+    };
 
-		init : function() {
+    WS.prototype.init = function () {
 
-//			if (autoReconnect !== undefined) {
-//				this.autoReconnect = autoReconnect;
-//			}
+        var that = this;
 
-			var that = this;
+        if (this.verbose)
+            console.log("WebSocket: " + that.id + " connecting to " + this.address);
 
-			if (this.verbose) {
-				console.log("WebSocket: " + that.id + " connecting to " + this.address);
-			}
+        try {
+            this.wsSocket = window['MozWebSocket'] ? new MozWebSocket(this.address) : new WebSocket(this.address);
+        } catch (e) {
+            console.error("WebSocket: " + that.id + " cannot connect to WebSocket at " + this.address);
+        }
 
-			try {
-				this.wsSocket = window['MozWebSocket'] ? new MozWebSocket(this.address) : new WebSocket(this.address);
-				var that = this;
-				this.wsSocket.onopen = function() {
-					if (that.verbose) {
-						console.log("WebSocket: " + that.id + " is opened ", that.wsSocket);
-					}
-					that.emit({
-						type : 'open',
-						content : that
-					});
+        if (this.wsSocket !== undefined) {
 
-				};
-				this.wsSocket.onerror = function(event) {
-					if (that.verbose) {
-						console.error("WebSocket: " + that.id + " has had an error. ", that.wsSocket);
-					}
-				};
-				this.wsSocket.onmessage = function(event) {
+            this.wsSocket.onopen = function () {
 
-					if (that.verbose) {
-						console.log("WebSocket: " + that.id + " has received ", event);
-					}
+                if (that.verbose)
+                    console.log("WebSocket: " + that.id + " is opened ", that.wsSocket);
 
-					var data = JSON.parse(event.data);
+                that.emit({
+                    type : 'open',
+                    content : that
+                });
 
-					if ((data === undefined) || (data.error)) {
-						if (that.verbose) {
-							console.error("WebSocket: " + that.id + " data error -> ", event.data);
-						}
-						that.emit({
-							type : 'error',
-							content : event
-						});
-						that.close();
+            };
+            this.wsSocket.onerror = function (event) {
+                if (that.verbose)
+                    console.error("WebSocket: " + that.id + " has had an error. ", that.wsSocket);
+            };
+            this.wsSocket.onmessage = function (event) {
 
-					} else {
+                var data = JSON.parse(event.data);
 
-						var msg = jQuery.parseJSON(event.data);
+                if (that.verbose)
+                    console.log("WebSocket: " + that.id + " has received ", event);
 
-						if (msg.ping != undefined) {
+                if ((data === undefined) || (data.error)) {
+                    if (that.verbose) {
+                        console.error("WebSocket: " + that.id + " data error -> ", event.data);
+                    }
+                    that.emit({
+                        type : 'error',
+                        content : event
+                    });
+                    that.close();
 
-							if ($(that.placeholderCssSel).length > 0) {
-								try {
-									clearTimeout(that.hangOutCheck);
-								} catch (err) {
-									if (that.verbose) {
-										console.error("ERROR -> ", err);
-									}
-								}
-								that.hangOutCheck = setTimeout(function() {
-									that.init();
-									// that.close()
-								}, 1000)
-								this.send(that.pong);
-							} else {
+                } else {
 
-								this.close();
-							}
+                    var msg = jQuery.parseJSON(event.data);
 
-						} else {
-							that.emit({
-								type : 'message',
-								content : msg
-							});
-						}
-					}
-				};
-				this.wsSocket.onclose = function() {
-					if (that.verbose) {
-						console.log("WebSocket: " + that.id + " closed by server. Placeholder: " + that.placeholderCssSel);
-					}
-					that.emit({
-						type : 'close',
-						content : that
-					});
-					if ($(that.placeholderCssSel).length > 0) {
-						// reactivate connection
-						setTimeout(function() {
-							that.init();
-						}, 500);
-					}
-				};
-			} catch (err) {
-				if (that.verbose) {
-					console.error("WebSocket: " + that.id + " cannot connect to WebSocket at " + this.address);
-					console.error("WebSocket: " + that.id + " error -> ", err);
-				}
-			}
+                    if (msg.ping != undefined) {
+                        if ($(that.placeholderCssSel).length > 0) {
+                            clearTimeout(that.hangOutCheck);
+                            that.hangOutCheck = setTimeout(function () {
+                                that.init();
+                            }, TIMEOUT)
+                            that.wsSocket.send(that.pong);
+                        } else {
+                            that.wsSocket.close();
+                        }
+                    } else {
+                        that.emit({
+                            type : 'message',
+                            content : msg
+                        });
+                    }
+                }
+            };
+            this.wsSocket.onclose = function () {
+                if (that.verbose)
+                    console.log("WebSocket: " + that.id + " closed by server. Placeholder: " + that.placeholderCssSel);
 
-		},
+                that.emit({
+                    type : 'close',
+                    content : that
+                });
 
-		send : function(msg) {
-			var that = this;
-			if (this.wsSocket !== null && this.wsSocket !== undefined) {
-				try {
-					if (that.verbose) {
-						console.log("WebSocket: " + this.id + " is sending ", msg);
-					}
-					this.wsSocket.send(JSON.stringify(msg));
-				} catch (e) {
-					if (that.verbose) {
-						console.warn("WebSocket: " + this.id + " reported error ", e);
-					}
-				}
-			} else {
-				if (that.verbose) {
-					console.error("WebSocket: " + this.id + " is not ready, maybe missing 'init'? ", this);
-				}
-			}
-		}
-	}
+                if ($(that.placeholderCssSel).length > 0) {
+                    // reactivate connection
+                    setTimeout(function () {
+                        that.init();
+                    }, TIMEOUT);
+                }
+            };
+        }
+    };
 
-	return WS;
+    WS.prototype.send = function (msg) {
+        if (this.isReady()) {
+            if (this.verbose)
+                console.log("WebSocket: " + this.id + " is sending ", msg);
+
+            this.wsSocket.send(JSON.stringify(msg));
+
+        } 
+        else 
+        {
+
+            if (this.verbose)
+                console.warn("WebSocket: " + this.id + " is not ready, maybe missing 'init'? ", this);
+
+        }
+    };
+
+    return WS;
 })
